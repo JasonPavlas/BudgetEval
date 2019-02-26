8b0468b0d50bb48e6f6549d9dfc4d46ffc9ad2a1
JasonPavlas <30872585+JasonPavlas@users.noreply.github.com>
2019-02-25 22:06:56 -0500

    Add project files.

---
+++ Budget/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transaction = ProcessFile("USAATransactions.csv");
+
+            var query = transaction.OrderBy(t => t.Description);
+
+            foreach (var line in query)
+            {
+                Console.WriteLine($"{line.Amount} \tfrom\t{line.Description}");
+            }
+
+            Console.WriteLine("\n\n\n");
+
+
+            var costQuerey = transaction.OrderBy(t => t.Amount);
+
+            foreach (var line in costQuerey)
+            {
+                Console.WriteLine($"{line.Amount} \tfrom\t{line.Description} \ton\t {line.Date}");
+            }
+
+            var dateQuerey =
+                    from line in transaction
+                    orderby line.Date
+                    select line;
+
+
+            foreach (var line in dateQuerey)
+            {
+                Console.WriteLine($"{line.Date}");
+            }
+
+
+            Console.ReadKey();
+        }
+
+
+        /*
+         *Method passed the path of the CSV and returns a Generic List of
+         * type Transaction
+        */
+        private static List<Transaction> ProcessFile(string path)
+        {
+            return
+                 File.ReadAllLines(path)
+                     .Where(line => line.Length > 1)
+                     .Select(Transaction.parseFromCSV)
+                     .ToList();
+
+        }
+    }
+
+}
---
+++ Budget/Transaction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudget
+{
+    class Transaction
+    {
+
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Amount { get; set; }
+
+
+
+
+        internal static Transaction parseFromCSV(string line)
+        {
+            var columns = line.Split(',');
+
+            //Get the date into an array
+            var _date = columns[0].Split('/');
+            
+            //Converts dates from string to int
+            int month = int.Parse(_date[0]);
+            int day = int.Parse(_date[1]);
+            int year = int.Parse(_date[2]);
+
+            //Do some work on Amount
+            double amount = double.Parse(columns[3]);
+
+            return new Transaction
+            {
+                Date = (string.Format("{0:00}", month) + "/" + string.Format("{0:00}", day) + "/" + string.Format("{0:00}", year)),
+                Description = columns[1],
+                Type = columns[2],
+                Amount = (string.Format("{0:0000.00}", amount.ToString("C")) )
+ 
+            };
+        }
+
+        
+    }
+}
