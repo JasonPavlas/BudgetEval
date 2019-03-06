23ba67234d7e30696d7ec8487bb7fbd3221359cd
Jason Pavlas <jasonpavlas@icloud.com>
2019-03-06 17:07:32 -0500

    Updated variable names and Time Date method

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -13,43 +13,14 @@ namespace PersonalBudget
         {
             var transaction = ProcessFile("USAATransactions.csv");
 
-            var query = transaction.OrderBy(t => t.Description);
-
-            foreach (var line in query)
-            {
-                Console.WriteLine($"{line.Amount} \tfrom\t{line.Description}");
-            }
-
-            Console.WriteLine("\n\n\n");
-
-
-            var costQuerey = transaction.OrderBy(t => t.Amount);
-
-            foreach (var line in costQuerey)
-            {
-                Console.WriteLine($"{line.Amount} \tfrom\t{line.Description} \ton\t {line.Date}");
-            }
-
-            var dateQuerey =
-                    from line in transaction
-                    orderby line.Date
-                    select line;
-
-
-            foreach (var line in dateQuerey)
-            {
-                Console.WriteLine($"{line.Date}");
-            }
+            sortByTransAmount(transaction);
+            sortByTransDescription(transaction);
 
 
             Console.ReadKey();
         }
 
-
-        /*
-         *Method passed the path of the CSV and returns a Generic List of
-         * type Transaction
-        */
+        
         private static List<Transaction> ProcessFile(string path)
         {
             return
@@ -59,6 +30,28 @@ namespace PersonalBudget
                      .ToList();
 
         }
+
+        private static void sortByTransAmount(List<Transaction> transaction)
+        {
+            var querey = transaction.OrderBy(t => t.TransAmount);
+
+            foreach (var line in querey)
+            {
+                Console.WriteLine($"{line.TransAmount}\tfrom\t{line.TransDescription} \ton\t {line.TransDate}");
+            }
+        }
+
+        private static void sortByTransDescription(List<Transaction> transaction)
+        {
+            var query = transaction.OrderBy(t => t.TransDescription);
+
+            foreach (var line in query)
+            {
+                Console.WriteLine($"{line.TransAmount} \tfrom\t{line.TransDescription}");
+            }
+        }
+
+       
     }
 
 }
--- Budget/Transaction.cs
+++ Budget/Transaction.cs
@@ -9,38 +9,31 @@ namespace PersonalBudget
     class Transaction
     {
 
-        public string Date { get; set; }
-        public string Description { get; set; }
-        public string Type { get; set; }
-        public string Amount { get; set; }
+        public DateTime TransDate { get; set; }
+        public string TransDescription { get; set; }
+        public string TransType { get; set; }
+        public double TransAmount { get; set; }
 
 
 
 
         internal static Transaction parseFromCSV(string line)
         {
+           
             var columns = line.Split(',');
 
-            //Get the date into an array
-            var _date = columns[0].Split('/');
             
-            //Converts dates from string to int
-            int month = int.Parse(_date[0]);
-            int day = int.Parse(_date[1]);
-            int year = int.Parse(_date[2]);
-
-            //Do some work on Amount
-            double amount = double.Parse(columns[3]);
-
             return new Transaction
             {
-                Date = (string.Format("{0:00}", month) + "/" + string.Format("{0:00}", day) + "/" + string.Format("{0:00}", year)),
-                Description = columns[1],
-                Type = columns[2],
-                Amount = (string.Format("{0:0000.00}", amount.ToString("C")) )
- 
+                TransDate = DateTime.Parse(columns[0]),
+                TransDescription = columns[1],
+                TransType = columns[2],
+                TransAmount = double.Parse(columns[3])
+               
             };
         }
+
+        
 
         
     }
